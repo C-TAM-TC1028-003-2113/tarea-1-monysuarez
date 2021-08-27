2cce9b0c049278cb7f970b8a9147f3b5a9f605cf
monysuarez <89654856+monysuarez@users.noreply.github.com>
2021-08-27 12:25:14 -0500

    Add files via upload

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    nuevos= int(input("Dame la cantidad de juegos nuevos: "))
+    usados = int(input("Dame la cantidad de juegos usados: "))
 
+    costonue = 1000*nuevos
+    costousados = 350 * usados
+    
+    costototal = costonue + costousados
+    print("El total de la compra es:",costototal)
 
 if __name__ == '__main__':
     main()

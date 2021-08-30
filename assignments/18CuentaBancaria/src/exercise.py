d64b172e0c7ed674285368e050408d58a94b1ac6
monysuarez <89654856+monysuarez@users.noreply.github.com>
2021-08-29 21:10:48 -0500

    Add files via upload

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    ant = float(input("Dame el saldo del mes anterior: "))
+    ingresos = float(input("Dame los ingresos: "))
+    egresos = float(input("Dame los egresos: "))
+    cheques= int(input("Dame el número de cheques: "))
 
+    costocheques= cheques * 13
+    utilidad = ingresos - egresos
+    totalmedio = ant + utilidad - costocheques
+    total = totalmedio - (totalmedio * 0.075)
 
+    print("El saldo final de la cuenta es:",total)
 if __name__ == '__main__':
     main()

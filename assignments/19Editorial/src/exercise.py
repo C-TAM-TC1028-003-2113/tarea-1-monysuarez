f3e2bedfdf9d98ac10178827f9382de64cf89ca5
monysuarez <89654856+monysuarez@users.noreply.github.com>
2021-08-29 22:32:41 -0500

    Add files via upload

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    import math
+    numpal = int(input("Dame el número de palabras: "))
+    
+    totalpag = math.ceil(numpal/ 475)
+    total = totalpag * 60
+    descuento = total -(total * 0.1)
+    
+    print("El costo de la publicación es:", descuento)
 
 
 if __name__ == '__main__':
